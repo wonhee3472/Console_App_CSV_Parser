246a2771fbee72efd7f9c0bf0dbcd1e9151e74ee
Jason Yun <56327729+wonhee3472@users.noreply.github.com>
2023-06-13 14:30:41 -0500

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+// Installed LINQtoCSV library to read a CSV file
+using LINQtoCSV;
+
+namespace CSVDemo
+{
+    [Serializable]
+    public class Employee
+    {
+        [CsvColumn(Name = "Employee ID")]
+        public string EmployeeID { get; set; }
+        
+        [CsvColumn(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [CsvColumn(Name = "Job Title")]
+        public string JobTitle { get; set; }
+
+        [CsvColumn(Name = "Department")]
+        public string Department { get; set; }
+
+        [CsvColumn(Name = "Business Unit")]
+        public string BusinessUnit { get; set; }
+
+        [CsvColumn(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [CsvColumn(Name = "Ethnicity")]
+        public string Ethnicity { get; set; }
+
+        [CsvColumn(Name = "Age")]
+        public string Age { get; set; }
+
+        [CsvColumn(Name = "Hire Date")]
+        public string HireDate { get; set; }
+
+        [CsvColumn(Name = "Annual Salary")]
+        public string AnnualSalary { get; set; }
+
+        [CsvColumn(Name = "Bonus %")]
+        public string Bonus { get; set; }
+
+        [CsvColumn(Name = "Country")]
+        public string Country { get; set; }
+
+        [CsvColumn(Name = "City")]
+        public string City { get; set; }
+
+        [CsvColumn(Name = "Exit Date")]
+        public string ExitDate { get; set; }
+        
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// Installed LINQtoCSV library to read a CSV file
+using LINQtoCSV;
+using System;
+using System.Collections.Generic;
+
+namespace CSVDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCsvFile();
+            Console.ReadKey();
+        }
+
+        private static void ReadCsvFile()
+        {
+            var csvFileDescription = new CsvFileDescription
+            {
+                FirstLineHasColumnNames = true,
+                IgnoreUnknownColumns = true,
+                SeparatorChar = ',',
+                UseFieldIndexForReadingData = false
+            };
+
+            var csvContext = new CsvContext();
+            var employees = csvContext.Read<Employee>("employees.csv", csvFileDescription);
+
+            var departmentCounts = new Dictionary<string, int>();
+
+            foreach(var employee in employees)
+            {
+                if (departmentCounts.ContainsKey(employee.Department))
+                {
+                    departmentCounts[employee.Department]++;
+                }
+                else
+                {
+                    departmentCounts[employee.Department] = 1;
+                }
+            }
+
+            foreach (var department in departmentCounts)
+            {
+                Console.WriteLine($"Department: {department.Key}, Count: {department.Value}");
+            }
+        }
+    }
+}
